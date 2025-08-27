ab1439263093df373cecdd5ab726d9becb937b5a
Ayush Negi <ayushpro111@gmail.com>
2025-08-27 14:28:04 +0530

    Change MongoDB connection string in db.js
    Updated MongoDB connection string in db.js.

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const connectDB = async()=>{
     try{
         //mongodb connection string "process.env.MONGO_URI"
-        const con = await mongoose.connect(process.env.MONGO_URI,{
+        const con = await mongoose.connect("mongodb+srv://ayushpro111:547579ayushnegi@cluster0.yexp9kg.mongodb.net/",{
             useNewUrlParser:true,
             useUnifiedTopology:true,
         })
